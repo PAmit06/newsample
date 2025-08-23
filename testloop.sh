75fac627ae8587bec76f6d14178f07dd0c8e760c
PAmit06 <pac2409@gmail.com>
2025-08-23 16:22:35 +0530

    Update testloop.sh

--- testloop.sh
+++ testloop.sh
@@ -4,11 +4,18 @@ do
   echo "Number: $i"
 done
 
-for i in (1..5}
+for i in {1..5}
 do
   echo "iteration: $i"
 done
 
+#while loop
 
+count=1
+while [$count -le 5]
+do
+  echo "count:$count"
+  ((count++))
+done
 
 
